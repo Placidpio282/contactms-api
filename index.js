2e232c55ca6ae60fde23991d219792c43cd010a7
Placidpio282 <placidpio2024@gmail.com>
2026-04-18 19:30:51 +0530

    file changes

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ import { Router } from './routes/routes.js'
 
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["https://contactms-client2.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 dotenv.config({path: "./config/.env"})
 
 
